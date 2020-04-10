d2fe4a6bba14cf71ebed5bf74be8cc7394becd90
Jake Pogorelec <jakepogo1@yahoo.com>
2020-04-09 21:36:58 -0500

    Updates

---
+++ precache-manifest.388496b2ff8f7741f3dd2c728e35041b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3dd790240678f3818b6c148b207bf1d",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b628433163b8d524bb79",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "b43661e1600c758f8165",
+    "url": "/robofriends/static/css/main.0ccff83f.chunk.css"
+  },
+  {
+    "revision": "b628433163b8d524bb79",
+    "url": "/robofriends/static/js/2.79e767d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.79e767d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43661e1600c758f8165",
+    "url": "/robofriends/static/js/main.9e04d2c8.chunk.js"
+  },
+  {
+    "revision": "dab07940c1980e5aab1e",
+    "url": "/robofriends/static/js/runtime-main.a4afcfec.js"
+  },
+  {
+    "revision": "1b690dbf848d90ccea78a4df6c370de7",
+    "url": "/robofriends/static/media/capture_it-webfont.1b690dbf.woff"
+  },
+  {
+    "revision": "339423c435eecd9f1ec69562c366f43e",
+    "url": "/robofriends/static/media/capture_it-webfont.339423c4.ttf"
+  },
+  {
+    "revision": "3e5269e8029fb2b3c274e8c7d9d12ccb",
+    "url": "/robofriends/static/media/capture_it-webfont.3e5269e8.eot"
+  },
+  {
+    "revision": "bde394bab3c46c73d8f3f09c41abda53",
+    "url": "/robofriends/static/media/capture_it-webfont.bde394ba.woff2"
+  },
+  {
+    "revision": "f05bea60748de0ab819edcaa2d489886",
+    "url": "/robofriends/static/media/capture_it-webfont.f05bea60.svg"
+  }
+]);
